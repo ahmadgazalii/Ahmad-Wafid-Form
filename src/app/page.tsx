2802175e28bc15b65e135a0df179eb8d931f7cc1
ahmadgazalii <aghazali789@gmail.com>
2024-09-04 02:00:50 -0700

    fix build error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
-import Image from "next/image";
 import Form from "./components/Form";
 import Navbar from "./components/Navbar";
-import FormStoreData from "./components/FormStoreData";
 import Footer from "./components/Footer";
 
 export default function Home() {
