42ad82c529e0a7e1a52f207f36c3487d793a52d9
ahmadgazalii <aghazali789@gmail.com>
2024-09-06 11:49:35 +0500

    add border

--- src/app/data/page.tsx
+++ src/app/data/page.tsx
@@ -43,57 +43,133 @@ async function Data() {
     //   ))}
     // </div>
     <div className="w-full overflow-x-auto">
-    <table className="min-w-full divide-y divide-gray-200">
-      <thead className="bg-gray-50">
-        <tr>
-          <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Id</th>
-          <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Country</th>
-          <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">City</th>
-          <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Travel To</th>
-          <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">First Name</th>
-          <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Last Name</th>
-          <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Date of Birth</th>
-          <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Marital Status</th>
-          <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Passport No.</th>
-          <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Confirm Passport No.</th>
-          <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Passport Issue Date</th>
-          <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Passport Issue Place</th>
-          <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Passport Expiry Date</th>
-          <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Visa Type</th>
-          <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Email</th>
-          <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Phone No.</th>
-          <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">National ID</th>
-          <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Position Applied For</th>
-          <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Other</th>
-        </tr>
-      </thead>
-      <tbody className="bg-white divide-y divide-gray-200">
-        {data.data?.map((elem: any) => (
-          <tr key={elem.id}>
-            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{elem.id}</td>
-            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{elem.country}</td>
-            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{elem.city}</td>
-            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{elem.countrytravelto}</td>
-            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{elem.firstname}</td>
-            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{elem.lastname}</td>
-            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{elem.dateofbirth}</td>
-            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{elem.martialstatus}</td>
-            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{elem.passportno}</td>
-            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{elem.confirmpassportno}</td>
-            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{elem.passportissuedate}</td>
-            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{elem.passportissueplace}</td>
-            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{elem.passportexpirydate}</td>
-            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{elem.visatype}</td>
-            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{elem.email}</td>
-            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{elem.phoneno}</td>
-            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{elem.nationalid}</td>
-            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{elem.postionappliedfor}</td>
-            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{elem.other}</td>
+      <table className="min-w-full divide-y divide-gray-200 border-2 border-solid border-black">
+        <thead className="bg-gray-50">
+          <tr>
+            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider border-2 border-solid border-black">
+              Id
+            </th>
+            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider border-2 border-solid border-black">
+              Country
+            </th>
+            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider border-2 border-solid border-black">
+              City
+            </th>
+            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider border-2 border-solid border-black">
+              Travel To
+            </th>
+            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider border-2 border-solid border-black">
+              First Name
+            </th>
+            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider border-2 border-solid border-black">
+              Last Name
+            </th>
+            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider border-2 border-solid border-black">
+              Date of Birth
+            </th>
+            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider border-2 border-solid border-black">
+              Marital Status
+            </th>
+            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider border-2 border-solid border-black">
+              Passport No.
+            </th>
+            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider border-2 border-solid border-black">
+              Confirm Passport No.
+            </th>
+            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider border-2 border-solid border-black">
+              Passport Issue Date
+            </th>
+            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider border-2 border-solid border-black">
+              Passport Issue Place
+            </th>
+            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider border-2 border-solid border-black">
+              Passport Expiry Date
+            </th>
+            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider border-2 border-solid border-black">
+              Visa Type
+            </th>
+            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider border-2 border-solid border-black">
+              Email
+            </th>
+            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider border-2 border-solid border-black">
+              Phone No.
+            </th>
+            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider border-2 border-solid border-black">
+              National ID
+            </th>
+            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider border-2 border-solid border-black">
+              Position Applied For
+            </th>
+            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider border-2 border-solid border-black">
+              Other
+            </th>
           </tr>
-        ))}
-      </tbody>
-    </table>
-  </div>
+        </thead>
+        <tbody className="bg-white divide-y divide-gray-200">
+          {data.data?.map((elem: any) => (
+            <tr key={elem.id}>
+              <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 border-2 border-solid border-black">
+                {elem.id}
+              </td>
+              <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 border-2 border-solid border-black">
+                {elem.country}
+              </td>
+              <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 border-2 border-solid border-black">
+                {elem.city}
+              </td>
+              <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 border-2 border-solid border-black">
+                {elem.countrytravelto}
+              </td>
+              <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 border-2 border-solid border-black">
+                {elem.firstname}
+              </td>
+              <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 border-2 border-solid border-black">
+                {elem.lastname}
+              </td>
+              <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 border-2 border-solid border-black">
+                {elem.dateofbirth}
+              </td>
+              <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 border-2 border-solid border-black">
+                {elem.martialstatus}
+              </td>
+              <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 border-2 border-solid border-black">
+                {elem.passportno}
+              </td>
+              <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 border-2 border-solid border-black">
+                {elem.confirmpassportno}
+              </td>
+              <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 border-2 border-solid border-black">
+                {elem.passportissuedate}
+              </td>
+              <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 border-2 border-solid border-black">
+                {elem.passportissueplace}
+              </td>
+              <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 border-2 border-solid border-black">
+                {elem.passportexpirydate}
+              </td>
+              <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 border-2 border-solid border-black">
+                {elem.visatype}
+              </td>
+              <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 border-2 border-solid border-black">
+                {elem.email}
+              </td>
+              <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 border-2 border-solid border-black">
+                {elem.phoneno}
+              </td>
+              <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 border-2 border-solid border-black">
+                {elem.nationalid}
+              </td>
+              <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 border-2 border-solid border-black">
+                {elem.postionappliedfor}
+              </td>
+              <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 border-2 border-solid border-black">
+                {elem.other}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 }
 
