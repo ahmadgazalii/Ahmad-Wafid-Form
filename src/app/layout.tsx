eb957e1f09a92d700323068890267f6e51f24853
khubaib-ak-47 <malikahma123d@gmail.com>
2024-09-12 23:11:30 +0500

    add spacing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={roboto.className}>
         
-      <Navbar />
+      <Navbar   />
 
         {children}
         
